672ea7eef08a905b7becd6557978fc41b7245e03
Lahirumadushan99 <149527519+Lahirumadushan99@users.noreply.github.com>
2023-11-01 13:43:58 +0530

    Quiz

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What is the color of ripe strawberries?",
+  answer: "Red",
+  options: [
+    "Blue",
+    "Yellow",
+    "Green",
+    "Red"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which animal can swim?",
+  answer: "Fish",
+  options: [
+    "Elephant",
+    "Fish",
+    "Giraffe",
+    "Kangaroo"
+  ]
+},
+  {
+  numb: 3,
+  question: "What do you use to write on a piece of paper?",
+  answer: "Pencil",
+  options: [
+    "Pen",
+    "Pencil",
+    "Crayon",
+    "Marker"
+  ]
+},
+  {
+  numb: 4,
+  question: "What do you wear on your head to keep warm in winter?",
+  answer: "Hat",
+  options: [
+    "Scarf",
+    "Gloves",
+    "Hat",
+    "Socks"
+  ]
+},
+  {
+  numb: 5,
+  question: "What do you call a baby dog?",
+  answer: "Puppy",
+  options: [
+    "Kitten",
+    "Cub",
+    "Fawn",
+    "Puppy"
+  ]
+},
+{
+  numb: 6,
+  question: "What is the opposite of 'day'?",
+  answer: "Night",
+  options: [
+    "Morning",
+    "Sunset",
+    "Dusk",
+    "Night"
+  ]
+},
+{
+  numb: 7,
+  question: "What do you use to blow up a balloon?",
+  answer: "Air",
+  options: [
+    "Water",
+    "Air",
+    "Fire",
+    "Sand"
+  ]
+},
+{
+  numb: 8,
+  question: "What is the largest planet in our solar system?",
+  answer: "Jupiter",
+  options: [
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter"
+  ]
+},
+{
+  numb: 9,
+  question: "Which bird can talk like humans?",
+  answer: "Parrot",
+  options: [
+    "Eagle",
+    "Ostrich",
+    "Parrot",
+    "Sparrow"
+  ]
+},
+{
+  numb: 10,
+  question: "What do you call a group of stars forming a pattern in the sky?",
+  answer: "Constellation",
+  options: [
+    "Galaxy",
+    "Constellation",
+    "Nebula",
+    "Comet"
+  ]
+}
+];
